0e03523d5fbb3bb7740f2b1dea7e5a345f2bd2fd
Edson Borges <edsonn.borgess@gmail.com>
2018-03-14 23:00:17 -0300

    Add files via upload

---
+++ TarefasApp/app/src/main/java/br/edsonnborgess/ifpi/tarefasapp/Formulario.java
@@ -0,0 +1,13 @@
+package br.edsonnborgess.ifpi.tarefasapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Formulario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+}
---
+++ TarefasApp/app/src/main/java/br/edsonnborgess/ifpi/tarefasapp/ListaTarefas.java
@@ -0,0 +1,13 @@
+package br.edsonnborgess.ifpi.tarefasapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaTarefas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_tarefas);
+    }
+}
